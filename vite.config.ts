2812fad5efcf0b42573822d3c113475a73fb38e1
pierrebaronqumu <pierre.baron@enghouse.com>
2025-09-25 09:56:14 +0100

    Introducing the env vars leakage

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    // we need this for vue-router and pinia to work in library mode
+    'process.env': process.env,
+  },
   plugins: [
     vue(),
     vueDevTools(),
